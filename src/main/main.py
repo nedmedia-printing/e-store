cc9b509e9a8592597681b834a75d0f97ed331992
mothetho <mobiusndou@gmail.com>
2024-09-27 10:13:59 +0200

    changed port

--- src/main/main.py
+++ src/main/main.py
@@ -2,16 +2,16 @@ from flask import Flask
 from src.config import config_instance
 
 from src.utils import template_folder, static_folder, upload_folder
-from src.controllers.projects import ProjectController
-
-projects = ProjectController()
 
 
-def bootstrap():
-    from src.database.projects import ProjectsORM
-    ProjectsORM.create_if_not_table()
-    from src.database.users import UsersORM
-    UsersORM.create_if_not_table()
+
+
+
+# /def bootstrap():
+#     # from src.database.projects import ProjectsORM
+#     # ProjectsORM.create_if_not_table()
+#     # from src.database.users import UsersORM
+#     # UsersORM.create_if_not_table()
 
 
 def create_app(config=config_instance()):
@@ -24,5 +24,5 @@ def create_app(config=config_instance()):
     with app.app_context():
         from src.routes.home import home_route
         app.register_blueprint(home_route)
-        bootstrap()
+        # bootstrap()
     return app
