7678459c21050a57c0dc58ac207f80acee91fdef
mothetho <mobiusndou@gmail.com>
2024-10-14 18:27:39 +0200

    Fix Declarative Imports

--- src/firewall/__init__.py
+++ src/firewall/__init__.py
@@ -123,7 +123,7 @@ class Firewall:
         for host in self.allowed_hosts:
             print(f"ALLOWED HOST : {host}")
             print(f"REQUEST HOST : {request.host}")
-            if not request.host.endswith(host):
+            if not request.host.startswith(host):
                 self._logger.error(f"Host not allowed: {request.host}")
                 abort(401, f'Host not allowed: {request.host}')
 
