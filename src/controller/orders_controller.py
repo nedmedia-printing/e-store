4379666cf1f8cfce097776c0eead12135b74a85b
mothetho <mobiusndou@gmail.com>
2024-10-14 13:14:31 +0200

    Updated Customer Details

--- src/controller/orders_controller.py
+++ src/controller/orders_controller.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import joinedload
 
 from src.controller import Controllers, error_handler
-from src.database.models.customer import Order
+from src.database.models.customer import Order, OrderStatus
 from src.database.sql.customer import OrderORM, CustomerORM, PaymentORM
 
 
@@ -41,3 +41,15 @@ class OrdersController(Controllers):
             order_orm = session.query(OrderORM).filter_by(order_id=order_id).first()
             return Order(**order_orm.to_dict())
 
+    async def get_refunds(self) -> list[Order]:
+        """
+
+        :return:
+        """
+        with self.get_session() as session:
+            order_orm_list = (
+                session.query(OrderORM)
+                .outerjoin(CustomerORM, OrderORM.customer_id == CustomerORM.uid)
+                .outerjoin(PaymentORM, OrderORM.order_id == PaymentORM.order_id)
+            ).filter_by(status=OrderStatus.RETURNED.value).all()
+            return [Order(**order_orm.to_dict()) for order_orm in order_orm_list]
--- src/database/sql/customer.py
+++ src/database/sql/customer.py
@@ -1,8 +1,8 @@
 from sqlalchemy import Column, String, inspect, Integer, Boolean, Text, ForeignKey, DateTime, Sequence
 from sqlalchemy.orm import relationship
-
 from src.database.constants import ID_LEN, NAME_LEN
 from src.database.sql import Base, engine
+from src.database.sql.products import ProductsORM
 
 
 class PaymentORM(Base):
@@ -10,13 +10,13 @@ class PaymentORM(Base):
     transaction_id = Column(String(ID_LEN), primary_key=True)
     receipt_number = Column(Integer, Sequence('invoice_number_seq'), autoincrement=True)
     order_id = Column(String(ID_LEN), ForeignKey('orders.order_id'))
-
     amount_paid = Column(Integer)
     date_paid = Column(DateTime)
     payment_method = Column(String(32))
     is_successful = Column(Boolean, default=False)
     month = Column(Integer)
     comments = Column(String(255))
+    order = relationship("OrderORM", back_populates="payments", uselist=False)
 
     @classmethod
     def create_if_not_table(cls):
@@ -28,7 +28,7 @@ class PaymentORM(Base):
         if inspect(engine).has_table(cls.__tablename__):
             cls.__table__.drop(bind=engine)
 
-    def to_dict(self):
+    def to_dict(self, include_relationships=False):
         return {
             'transaction_id': self.transaction_id,
             'order_id': self.order_id,
@@ -38,7 +38,8 @@ class PaymentORM(Base):
             'payment_method': self.payment_method,
             'is_successful': self.is_successful,
             'month': self.month,
-            'comments': self.comments
+            'comments': self.comments,
+            "order": self.order.to_dict() if self.order and include_relationships else None
         }
 
 
@@ -46,16 +47,16 @@ class CustomerORM(Base):
     __tablename__ = "customers"
     uid = Column(String(ID_LEN), ForeignKey('users.uid'), primary_key=True)
     order_count = Column(Integer)
-    name: str = Column(String(60))
+    name = Column(String(60))
     total_spent = Column(Integer)  # Stored in cents
     city = Column(String(255))
     last_seen = Column(DateTime)
     last_order_date = Column(DateTime)
     notes = Column(String(255))
 
-    # Relationship to Ordersproducts
-    orders = relationship("OrderORM", back_populates="customer")
-    user = relationship("UserORM", back_populates='customer')
+    # Relationship to Orders products
+    orders = relationship("OrderORM", back_populates="customer", uselist=True)
+    user = relationship("UserORM", back_populates='customer', uselist=False)
 
     @classmethod
     def create_if_not_table(cls):
@@ -67,7 +68,7 @@ class CustomerORM(Base):
         if inspect(engine).has_table(cls.__tablename__):
             cls.__table__.drop(bind=engine)
 
-    def to_dict(self):
+    def to_dict(self, include_relationships=False):
         return {
             "uid": self.uid,
             "name": self.name,
@@ -77,8 +78,8 @@ class CustomerORM(Base):
             "last_seen": self.last_seen,
             "last_order_date": self.last_order_date,
             "notes": self.notes,
-            "orders": [order.to_dict() for order in self.orders],
-            "user": self.user.to_dict() if self.user else None
+            "orders": [order.to_dict() for order in self.orders] if include_relationships else [],
+            "user": self.user.to_dict() if self.user else None if include_relationships else None
         }
 
 
@@ -92,7 +93,47 @@ class OrderORM(Base):
     date_paid = Column(DateTime)
 
     # Relationship to Customer
-    customer = relationship("CustomerORM", back_populates="orders")
+    customer = relationship("CustomerORM", back_populates="orders", uselist=False)
+    payments = relationship("PaymentORM", back_populates="order", uselist=True)
+    order_items = relationship("OrderItemsORM", back_populates="order", uselist=True)
+
+    @classmethod
+    def create_if_not_table(cls):
+        if not inspect(engine).has_table(cls.__tablename__):
+            cls.__table__.create(bind=engine)
+
+    @classmethod
+    def delete_table(cls):
+        if inspect(engine).has_table(cls.__tablename__):
+            cls.__table__.drop(bind=engine)
+
+    def to_dict(self, include_relationships=False):
+        return {
+            "order_id": self.order_id,
+            "customer_id": self.customer_id,
+            "order_date": self.order_date,
+            "total_amount": self.total_amount,
+            "status": self.status,
+            "date_paid": self.date_paid,
+            "customer": self.customer.to_dict() if self.customer and include_relationships else None,
+            "payments": [payment.to_dict() for payment in self.payments] if include_relationships else [],
+            "items": [item.to_dict() for item in self.order_items] if self.order_items else []
+        }
+
+
+class OrderItemsORM(Base):
+    __tablename__ = "order_items"
+    item_id = Column(String(ID_LEN), primary_key=True)
+    order_id = Column(String(ID_LEN), ForeignKey("orders.order_id"))
+    product_id = Column(String(ID_LEN), ForeignKey("products.product_id"))
+    quantity = Column(Integer, nullable=False)
+    price = Column(Integer, nullable=False)  # Price at the time of order, stored in cents
+
+    # Relationship to Order
+    order = relationship("OrderORM", back_populates="order_items")
+
+    # Relationship to Product
+    product = relationship("ProductsORM", uselist=False)
 
     @classmethod
     def create_if_not_table(cls):
@@ -106,10 +147,10 @@ class OrderORM(Base):
 
     def to_dict(self):
         return {
+            "item_id": self.item_id,
             "order_id": self.order_id,
-            "customer_id": self.customer_id,
-            "order_date": self.order_date,
-            "total_amount": self.total_amount,
-            "status": self.status,
-            "date_paid": self.date_paid
+            "product_id": self.product_id,
+            "quantity": self.quantity,
+            "price": self.price,
+            "product": self.product.to_dict() if self.product else None
         }
