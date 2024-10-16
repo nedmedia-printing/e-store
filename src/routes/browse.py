f3b5fccc74156e49818cf772283449fd99a3f327
mothetho <mobiusndou@gmail.com>
2024-10-16 14:23:01 +0200

    Updated Orders

--- src/routes/browse.py
+++ src/routes/browse.py
@@ -24,3 +24,20 @@ async def get_category(user: User, seo_slug: str):
     context = dict(category=category, user=user)
     return render_template('category_products.html', **context)
 
+
+@browse_route.get('/categories')
+@user_details
+async def browse(user: User, seo_slug: str):
+    """
+    Retrieve the category by slug and render the category products.
+
+    :param user: User object
+    :param seo_slug: SEO-friendly slug of the category
+    :return: Rendered template with the category products
+    """
+    # Fetch the category details and products
+    categories = await inventory_controller.get_product_categories()
+
+    context = dict(categories=categories, user=user)
+    return render_template('browse.html', **context)
+
