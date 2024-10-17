d923fe6329cf811443a4005452f7928f8793b109
mothetho <mobiusndou@gmail.com>
2024-10-17 15:23:56 +0200

    formalized cart

--- src/database/models/cart.py
+++ src/database/models/cart.py
@@ -19,18 +19,18 @@ class Cart(BaseModel):
         return sum(item.quantity for item in self.items)
 
     @property
-    def total_price(self) -> float:
+    def total_price(self) -> int:
         """Calculate total price for all items in the cart."""
-        return sum(item.total_price for item in self.items)
+        return sum(item.line_price for item in self.items)
 
-    def add_item(self, product_id: str, name: str, price: float, quantity: int = 1):
+    def add_item(self, product_id: str, quantity: int = 1):
         """Add an item to the cart."""
         for item in self.items:
             if item.product_id == product_id:
                 item.quantity += quantity
                 return
         # If item is not found, add a new item
-        new_item = CartItem(product_id=product_id, name=name, price=price, quantity=quantity)
+        new_item = CartItem(product_id=product_id, cart_id=self.cart_id, quantity=quantity)
         self.items.append(new_item)
 
     def remove_item(self, product_id: str):
@@ -40,6 +40,22 @@ class Cart(BaseModel):
     def clear_cart(self):
         """Clear all items from the cart."""
         self.items.clear()
+
+    def cart_summary(self):
+        """
+        Summarizes the cart details for easy checkout handling.
+        :return: dict containing cart summary.
+        """
+        return {
+            "uid": self.uid,
+            "cart_id": self.cart_id,
+            "created_at": self.created_at.isoformat(),
+            "converted_to_order": self.converted_to_order,
+            "converted_at": self.converted_at.isoformat() if self.converted_at else None,
+            "total_items": self.total_items,
+            "total_price": self.total_price,
+            "items": [item.item_summary() for item in self.items]
+        }
 
 
 class CartItem(BaseModel):
@@ -75,3 +91,15 @@ class CartItem(BaseModel):
     @property
     def inventory_count(self) -> int:
         return self.product.inventory_count
+
+    def item_summary(self):
+        return {
+            "item_id": self.item_id,
+            "cart_id": self.cart_id,
+            "product_id": self.product_id,
+            "quantity": self.quantity,
+            "item_price": self.item_price,
+            "line_price": self.line_price,
+            "name": self.name,
+            "description": self.description,
+            "inventory_count": self.inventory_count}
