84d142d0be1eff144abebd10b472f7d51bc95184
mothetho <mobiusndou@gmail.com>
2024-10-20 14:16:04 +0200

    speeded up cart items

--- src/controller/cart_controller.py
+++ src/controller/cart_controller.py
@@ -1,3 +1,5 @@
+from flask import Flask
+import asyncio
 from sqlalchemy.orm import joinedload
 
 from src.controller import Controllers, error_handler
@@ -8,49 +10,50 @@ from src.database.sql.cart import CartORM, CartItemORM
 class CartController(Controllers):
     def __init__(self):
         super().__init__()
+        self.__carts = []
+        self.__carts_dict = {}
+
+    def init_app(self, app: Flask):
+        asyncio.run(self.preload_carts())
 
     @error_handler
-    async def get_outstanding_customer_cart(self, uid: str) -> Cart | None:
-        """
-        Retrieves the outstanding customer cart with all linked products.
-        :param uid: User ID
-        :return: Cart instance or None if not found
-        """
+    async def preload_carts(self):
+        self.__carts = await self.get_all_carts()
+        self.__carts_dict = {cart.cart_id: cart for cart in self.__carts}
+
+    @error_handler
+    async def get_all_carts(self) -> list[Cart]:
+        """ Retrieves all carts from the database with linked items. """
         with self.get_session() as session:
-            cart_orm = (
+            carts_list_orm = (
                 session.query(CartORM)
                 .options(
                     joinedload(CartORM.items).joinedload(CartItemORM.product)
                 )
-                .filter_by(uid=uid, converted_to_order=False)
-                .first()
+                .all()
             )
-            if not cart_orm:
-                return None
-            self.logger.info(f"Cart ORM: {cart_orm.to_dict()}")
-            return Cart(**cart_orm.to_dict(include_relationships=True))
+            return [Cart(**cart.to_dict(include_relationships=True)) for cart in carts_list_orm]
+
+    @error_handler
+    async def get_outstanding_customer_cart(self, uid: str) -> Cart | None:
+        """ Retrieves the outstanding customer cart with all linked products. """
+        for cart in self.__carts:
+            if cart.uid == uid and not cart.converted_to_order:
+                return cart
+        return None
 
     @error_handler
     async def add_cart_item(self, cart_item: CartItem) -> CartItem:
-        """
-        Adds an item to the customer's cart.
-        :param cart_item: CartItem instance to be added
-        :return: Added CartItem instance
-        """
+        """ Adds an item to the customer's cart. """
         with self.get_session() as session:
-            # Check if the cart exists
             cart_orm = session.query(CartORM).filter_by(cart_id=cart_item.cart_id).first()
             if not cart_orm:
                 raise ValueError("Cart not found")
-
-            # Check if the item is already in the cart
             existing_item = session.query(CartItemORM).filter_by(cart_id=cart_item.cart_id,
                                                                  product_id=cart_item.product_id).first()
             if existing_item:
-                # Update quantity if item exists
                 existing_item.quantity += cart_item.quantity
             else:
-                # Add new item
                 new_cart_item = CartItemORM(
                     item_id=cart_item.item_id,
                     cart_id=cart_item.cart_id,
@@ -58,18 +61,13 @@ class CartController(Controllers):
                     quantity=cart_item.quantity
                 )
                 session.add(new_cart_item)
-
-            return cart_item
+        await self.preload_carts()  # Update preloaded carts after adding a new item
+        return cart_item
 
     @error_handler
     async def create_new_cart(self, cart: Cart) -> Cart:
-        """
-        Creates a new cart for the customer.
-        :param cart: Cart instance to be created
-        :return: Created Cart instance
-        """
+        """ Creates a new cart for the customer. """
         with self.get_session() as session:
-            # Create a new CartORM instance
             new_cart_orm = CartORM(
                 cart_id=cart.cart_id,
                 uid=cart.uid,
@@ -77,8 +75,6 @@ class CartController(Controllers):
                 converted_to_order=cart.converted_to_order,
                 converted_at=cart.converted_at
             )
-
-            # Add cart items to the CartORM instance
             for item in cart.items:
                 new_cart_item = CartItemORM(
                     item_id=item.item_id,
@@ -87,27 +83,17 @@ class CartController(Controllers):
                     quantity=item.quantity
                 )
                 new_cart_orm.items.append(new_cart_item)
-
-            # Add the new cart to the session
             session.add(new_cart_orm)
-
-            # Return the created cart
-            return cart
+        await self.preload_carts()  # Update preloaded carts after creating a new cart
+        return cart
 
     @error_handler
-    async def remove_cart_item(self, item_id: str):
-        """
-        Removes an item from the cart by product ID.
-        :param item_id:
-        :return: None
-        """
+    async def remove_cart_item(self, item_id: str) -> bool:
+        """ Removes an item from the cart by item ID. """
         with self.get_session() as session:
-            # Find the cart item
             cart_item = session.query(CartItemORM).filter_by(item_id=item_id).first()
             if not cart_item:
                 raise ValueError("Cart item not found")
-
-            # Remove the item
             session.delete(cart_item)
-            return True
-
+        await self.preload_carts()  # Update preloaded carts after removing an item
+        return True
